b1737b9c9487b60ccb461cd444127668db0aeb12
Prabhash Rai <raiprabh@groww.in>
2023-01-30 23:31:06 +0530

    added redux for state management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import Navbar from '@/Common Components/Navbar/Navbar'
+import Navbar from '@/Common/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { store } from '../Store/store';
+import { Provider } from 'react-redux';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
