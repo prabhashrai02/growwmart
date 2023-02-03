87d7120eeaee07796f0ff41c70d88dcfa0b8f597
Prabhash Rai <raiprabh@groww.in>
2023-02-03 12:53:09 +0530

    fixed navbar

--- src/Common/Navbar/index.tsx
+++ src/Common/Navbar/index.tsx
@@ -4,44 +4,36 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import style from './Navbar.module.css';
 import { getLocalData, setLocalData } from '@/utils/useLocalStorage';
-import { setData } from '@/Store/slices/cartSlice';
-import { CartState } from '@/UI/CartPage/Types';
+import { setDataFromLocal } from '@/Store/slices/cartSlice';
+import { setWishList } from '@/Store/slices/wishSlice';
+
+import Link from 'next/link';
+import { useLocalData } from '@/utils/customHooks';
 
 const Navbar = () => {
-  const dispatch = useDispatch();
+  const { cartSize } = useLocalData();
 
-  const cartSize = useSelector((state: RootState) => state.cart.value);
-  const cart = useSelector((state: RootState) => state.cart);
-  
-  const localCartData = getLocalData("cartState");
-
-  useEffect(() => {
-    localCartData ? dispatch(setData(JSON.parse(localCartData))) :
-    setLocalData<CartState>("cartState", cart)
-  }, []);
-  
-  useEffect(() => {
-    setLocalData<CartState>("cartState", cart)
-  }, [cartSize])
+  const cartURL = `../cart`;
 
   return (
     <div className={style.navbar32Bar}>
-
+      <Link href='../allproducts'>
       <div className={style.navbar45Left}>
         <img src='https://groww.in/logo-groww270.png' alt='logo' />
         <p className={style.navbar23Heading}>GrowwMart</p>
       </div>
+      </Link>
 
       <div className={style.navbar98Center}>
         <input type='text' className={style.navbar85Input} placeholder='Search for any Product' />
       </div>
 
-
+      <Link href={cartURL}>
       <div className={style.navbar44Right}>
         <p className={style.navbar75CartItems}>{cartSize}</p>
         <img className={style.navbar90Cart} src="https://img.icons8.com/ios/50/null/shopping-cart--v1.png" alt='Cart' />
       </div>
-
+      </Link>
     </div>
   )
 }
--- src/utils/customHooks.ts
+++ src/utils/customHooks.ts
@@ -2,6 +2,45 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { Product } from "@/UI/ProductPage/Types";
 import { getLocalData, setLocalData } from "./useLocalStorage";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/Store/store";
+import { CartState, WishState } from "@/Store/slices/Types";
+import { setDataFromLocal } from "@/Store/slices/cartSlice";
+import { setWishList } from "@/Store/slices/wishSlice";
+
+export const useLocalData = () => {
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state: RootState) => state.cart);
+  const wish: WishState = useSelector((state: RootState) => state.wish);
+  const cartSize = cart.cartProducts.length;
+  
+  const localCartData = getLocalData("cartState");
+  const localWishList = getLocalData("wishList");
+
+  useEffect(() => {
+    localCartData ? dispatch(setDataFromLocal(JSON.parse(localCartData))) :
+    setLocalData<CartState>("cartState", cart)
+
+    localWishList ? dispatch(setWishList(JSON.parse(localWishList))) :
+    setLocalData<Product[]>("wishList", wish.wishList)
+  }, []);
+  
+  useEffect(() => {
+    setLocalData<CartState>("cartState", cart)
+    setLocalData<Product[]>("wishList", wish.wishList)
+
+  }, [cartSize])
+  
+  useEffect(() => {
+    setLocalData<Product[]>("wishList", wish.wishList)
+
+  }, [wish])
+
+  return {
+    cartSize,
+  }
+}
 
 export const useProductArray = () => {
   const [productArray, setProductArray] = useState<Product[]>([]);
@@ -31,7 +70,6 @@ export const useProductArray = () => {
   }
 }
 
-
 export const useProduct = () => {
 
   const router = useRouter();
@@ -60,7 +98,7 @@ export const useProduct = () => {
 export const useFilters = () => {
 
   const [categories, setCategories] = useState<string[]>([]);
-  const [price, setPrice] = useState<number>(200);
+  const [price, setPrice] = useState<number>(1000);
 
   const fetchCategories = async () => {
     try {
