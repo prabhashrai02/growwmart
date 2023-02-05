a55159761e8f3482c677bbb11c39a4de92eef8fc
Prabhash Rai <raiprabh@groww.in>
2023-02-05 12:32:42 +0530

    fixed cart list page

--- src/UI/ProductListPage/index.tsx
+++ src/UI/ProductListPage/index.tsx
@@ -1,27 +1,37 @@
 import Card from '@/Common/Card';
 import Filters from '@/Common/Filters';
+import { setProductList } from '@/Store/slices/productSlice';
+import { RootState } from '@/Store/store';
 import { useProductArray } from '@/utils/customHooks';
 
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import style from './ProductListPage.module.css';
 
 
 const ProductListPage = () => {
+  const showProducts = useSelector((state: RootState) => state.product.showList);
 
+  const dispatch = useDispatch();
   const { productArray } = useProductArray();
+  
+    useEffect(() => {
+      productArray && dispatch(setProductList(productArray));
+    }, [productArray]);
 
   return (
     <>
       <div className={style.productlist43page}>
         <Filters />
-        <div className={style.productlist41alloproducts}>
-          {
-            productArray &&
-            productArray.map((item, index): ReactNode => {
-              return <Card data={item} key={index} />
-            })
-          }
+        <div className={style.product23All}>
+          <div className={style.productlist41allProducts}>
+            {
+              showProducts?.map((item, index): ReactNode => {
+                return <Card data={item} key={index} />
+              })
+            }
+          </div>
         </div>
       </div>
     </>
