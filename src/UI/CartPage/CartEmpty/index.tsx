561f53e3d3ad9b6b55bf6fb3ba369273a4b67490
Prabhash Rai <raiprabh@groww.in>
2023-02-05 12:21:43 +0530

    created empty cart screen

---
+++ src/UI/CartPage/CartEmpty/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './CartEmpty.module.css';
+
+const CartEmpty = () => {
+  return (
+    <div className={style.cartempty56EmptyBox}>
+      You don't have any item in cart, Please add something ;(
+    </div>
+  )
+}
+
+export default CartEmpty;
