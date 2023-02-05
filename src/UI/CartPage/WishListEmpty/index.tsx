aca0b8891d8d45d9c8d43a890fae1c0ab0af0441
Prabhash Rai <raiprabh@groww.in>
2023-02-05 12:32:07 +0530

    created empty wishlist page

---
+++ src/UI/CartPage/WishListEmpty/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './WishListEmpty.module.css';
+
+const WishListEmpty = () => {
+  return (
+    <div className={style.wish45EmptyWishList}>
+      You don't have any item in wish list, Please add something ;(
+    </div>
+  )
+}
+
+export default WishListEmpty;
