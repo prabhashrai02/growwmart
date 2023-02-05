f9c0eff99aacf833d0ae365dbef2bba6afe77b67
Prabhash Rai <raiprabh@groww.in>
2023-02-05 12:33:49 +0530

    created cart page

---
+++ src/UI/CartPage/index.tsx
@@ -0,0 +1,48 @@
+import Card from '@/Common/Card';
+import { RootState } from '@/Store/store';
+import React, { ReactNode, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import CartEmpty from './CartEmpty';
+import style from './CartPage.module.css';
+import WishListEmpty from './WishListEmpty';
+
+const CartPage = () => {
+
+  const cart = useSelector((state: RootState) => state.cart);
+  const cartProduct = cart.cartProducts;
+  const cartSize = cart.cartProducts.length;
+
+  const wishList = useSelector((state: RootState) => state.wish.wishList);
+  const wishListSize = wishList.length;
+    
+    return (
+        <div className={style.cart98CartPage}>
+            <div className={style.cart34Box}>
+                {
+                    cartSize ?
+                    cartProduct.map((item, index): ReactNode => {
+                        return <Card cartData={true} data={item.product} quantity={item.quantity} key={index} />;
+                    })
+                    :
+                    <CartEmpty />
+                }
+            </div>
+            <div className={style.cart34Box}>
+                {
+                    wishListSize ?
+                    wishList.map((item, index): ReactNode => {
+                        return (
+                            <div className={style.cart56WistList}>
+                                <Card productPage={true} data={item} key={index} />
+                            </div>
+                        )
+                    })
+                    :
+                    <WishListEmpty />
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CartPage;
