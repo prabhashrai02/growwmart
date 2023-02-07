f8eb685d0bcbdf3db7c07fef47569920ad063229
Prabhash Rai <raiprabh@groww.in>
2023-02-07 23:50:03 +0530

    fixed filter hook

--- src/Common/Filters/customFilterHooks.ts
+++ src/Common/Filters/customFilterHooks.ts
@@ -1,9 +1,8 @@
-import { setCategoriesList, updateShowList, updateSearchValue } from "@/Store/slices/productSlice";
+import { setCategoriesList, updateSearchValue } from "@/Store/slices/productSlice";
 import { RootState } from "@/Store/store";
 import router from "next/router";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { FilterData } from "./Types";
 
 export const useFilters = () => {
 
@@ -37,60 +36,18 @@ export const useFilters = () => {
   }
 }
 
-
 export const useSyncFilter = () => {
 
   const { categories, price, setPrice } = useFilters();
   const dispatch = useDispatch();
+  const searchValue = useSelector((state: RootState) => state.product.searchValue);
 
   useEffect(() => {
     categories && dispatch(setCategoriesList(categories));
   }, [categories])
 
-  const { value, sort, selectCategories, priceFilter } = router.query;
-
-  const [searchValue, setSearchValue] = useState<string>('');
-  const [sortValue, setSortValue] = useState<string>('');
-  const [selectedCategories, setSelectedCategories] = useState<Set<FormDataEntryValue>>();
-
-
   const skeletonArray = [1, 2, 3, 4];
 
-  const searchQuery = useSelector((state: RootState) => state.product.searchValue);
-
-  useEffect(() => {
-    setSearchValue(searchQuery);
-  }, [searchQuery]);
-
-  useEffect(() => {
-    const searchValueLength = value ? value.length : 0;
-    value && setSearchValue(JSON.stringify(value).substring(1, 1 + searchValueLength));
-
-    const sortLength = sort ? sort.length : 0;
-    const sortBy = sort ? JSON.stringify(sort).substring(1, 1 + sortLength) : '';
-
-    const categories = JSON.stringify(selectCategories);
-    const catArray = categories && categories.substring(1, categories.length - 1).split(',').filter((ele) => ele);
-    const filterCategory: Set<FormDataEntryValue> = categories ? new Set([...catArray]) : new Set();
-    const urlPrice = Number(priceFilter);
-
-    setSortValue(sortBy);
-    urlPrice ? setPrice(urlPrice) : setPrice(1000);
-    setSelectedCategories(filterCategory);
-
-  }, [router.query])
-
-  useEffect(() => {
-    const filter: FilterData = {
-      sort: sortValue,
-      selectCategory: selectedCategories,
-      priceFilter: price,
-      searchValue: searchValue
-    }
-
-    dispatch(updateShowList(filter));
-
-  }, [price, selectedCategories, searchValue, sortValue])
 
   const takeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = Number(event.currentTarget.value);
@@ -98,25 +55,8 @@ export const useSyncFilter = () => {
   }
 
   const resetFilter = () => {
-
-    const filterCategory: Set<FormDataEntryValue> = new Set();
-
-    const sort = '';
-
-    setPrice(1000);
-
-    const filter: FilterData = {
-      priceFilter: 1000,
-      sort: sort,
-      selectCategory: filterCategory,
-      searchValue: searchValue,
-    }
-
     dispatch(updateSearchValue(''));
-    dispatch(updateShowList(filter));
-
-
-    router.push(`search?value=&sort=${sort}&selectCategories=&priceFilter=${1000}`);
+    router.push(`search?value=&sort=&selectCategories=&priceFilter=${1000}`);
   }
 
   const filterChanges = (event: React.FormEvent<HTMLFormElement>) => {
@@ -129,6 +69,7 @@ export const useSyncFilter = () => {
 
     const categoriesFilter = JSON.stringify([...filterCategory].join(','));
     const urlFilter = categoriesFilter.substring(1, categoriesFilter.length - 1);
+
     router.push(`search?value=${searchValue}&sort=${sort}&selectCategories=${urlFilter}&priceFilter=${price}`);
   }
 
