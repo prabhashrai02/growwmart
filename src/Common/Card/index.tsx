f4421067b2a0a477c8a1e8f66c5f75d46b459d92
Prabhash Rai <raiprabh@groww.in>
2023-02-09 00:06:54 +0530

    fix card

--- src/Common/Card/index.tsx
+++ src/Common/Card/index.tsx
@@ -13,6 +13,8 @@ import CardBookmark from './CardBookmark';
 import CardProductOffers from './CardProductOffers';
 import CardCartQuantity from './CardCartQuantity';
 import CardWishListButton from './CardWishListButton';
+import CardPrice from './CardPrice';
+import CardSale from './CardSale';
 
 const Card = (props: CardProps) => {
     const { data, cartPage, productPage, quantity, showDescription, wishList } = props;
@@ -21,11 +23,8 @@ const Card = (props: CardProps) => {
     const imageURL = `${data?.image}`;
 
     const modifyCardProductStyle = productPage ? `${style.card75Product}` : cartPage ? `${style.card77Product}` : "";
-
     const modifyCardAlignment = productPage ? `${style.card45ProductDetails}` : cartPage ? `${style.card44ProductDetails}` : "";
-
     const cardProductWithButton = productPage ? `${style.card12ProductImageButtons}` : "";
-
     const modifyProductDetail = productPage ? `${style.card67ProductDescription}` : cartPage ? `${style.card67ProductDescription}` : "";
 
     const productURL = `../product/${data?.id}`;
@@ -35,9 +34,9 @@ const Card = (props: CardProps) => {
             data && (
                 <Link href={productURL}>
                     <div className={`${style.card23ProductDetails} ${modifyCardAlignment}`}>
-                        <div>
+                        <div className={style.card12SaleBookmark}>
                             <CardBookmark data={data} />
-
+                            <CardSale data={data} />
                             <div className={`${style.card89ImageHolder} ${cardProductWithButton}`}>
                                 <Image loader={() => imageURL} src={imageURL} alt={data.title} unoptimized={true} width={100} height={100} />
                                 <CardButtons check={productPage} data={data} />
@@ -48,7 +47,7 @@ const Card = (props: CardProps) => {
                             <CardTitle productPage={productPage} cartPage={cartPage} title={data.title} />
                             <p>{data.category}</p>
                             <div className={style.card98ProductPriceRating}>
-                                <span className={style.cart78ProductPrice}><h4>&#x20b9; {data.price}</h4></span>
+                                <CardPrice data={data} />
                                 <CardRating data={data.rating} />
                             </div>
                             <CardDesc check={showDescription} data={data.description} />
