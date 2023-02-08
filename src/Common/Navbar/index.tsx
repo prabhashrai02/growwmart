62616bacbad3c0965e117fc419dcf81c3f5963ee
Prabhash Rai <raiprabh@groww.in>
2023-02-09 00:05:25 +0530

    added search icon

--- src/Common/Navbar/index.tsx
+++ src/Common/Navbar/index.tsx
@@ -5,6 +5,9 @@ import style from './Navbar.module.css';
 import Link from 'next/link';
 import { useLocalData } from '@/utils/customLocalSyncHook';
 import { useNavbar } from './customNavbarHooks';
+import Image from 'next/image';
+
+import search from '@/Assets/search.svg';
 
 const Navbar = () => {
 
@@ -14,21 +17,22 @@ const Navbar = () => {
   return (
     <div className={style.navbar32Bar}>
       <Link href='../allproducts'>
-      <div className={style.navbar45Left}>
-        <img src='https://groww.in/logo-groww270.png' alt='logo' />
-        <p className={style.navbar23Heading}>GrowwMart</p>
-      </div>
+        <div className={style.navbar45Left}>
+          <img src='https://groww.in/logo-groww270.png' alt='logo' />
+          <p className={style.navbar23Heading}>GrowwMart</p>
+        </div>
       </Link>
 
       <div className={style.navbar98Center}>
         <input type='text' className={style.navbar85Input} value={searchValue} placeholder='Search for any Product' onChange={(event) => handleChangeInput(event)} />
+        <Image src={search} alt='search' />
       </div>
 
       <Link href={cartURL}>
-      <div className={style.navbar44Right}>
-        <p className={style.navbar75CartItems}>{cartSize}</p>
-        <img className={style.navbar90Cart} src="https://img.icons8.com/ios/50/null/shopping-cart--v1.png" alt='Cart' />
-      </div>
+        <div className={style.navbar44Right}>
+          <p className={style.navbar75CartItems}>{cartSize}</p>
+          <img className={style.navbar90Cart} src="https://img.icons8.com/ios/50/null/shopping-cart--v1.png" alt='Cart' />
+        </div>
       </Link>
     </div>
   )
