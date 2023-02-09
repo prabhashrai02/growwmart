5a22d745ad8bc251a9800775b2d8915ec56a5132
Prabhash Rai <raiprabh@groww.in>
2023-02-09 13:34:41 +0530

    added footer section

--- src/Common/Footer/index.tsx
+++ src/Common/Footer/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import style from 'Footer.module.css';
+import style from './Footer.module.css';
 
 const Footer = () => {
-  return (
-    <div className={style.footer67Section}>
-      
-    </div>
-  )
+    return (
+        <div className={style.footer67Section}>
+            <span>
+                Quick links
+            </span>
+            <div className={style.footer80Policies}>
+                <span>
+                    About Us
+                </span>
+                <span>
+                    Terms of Service
+                </span>
+                <span>
+                    Privacy Policy
+                </span>
+                <span>
+                    Cancellation & Refund Policy
+                </span>
+                <span>
+                    Shipping Policy
+                </span>
+            </div>
+            <div className={style.footer79TrackContactBlog}>
+                <span>
+                    Track My Order
+                </span>
+                <span>
+                    Contact Us
+                </span>
+                <span>
+                    Our Blog
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,14 @@ import type { AppProps } from 'next/app'
 
 import { store } from '../Store/store';
 import { Provider } from 'react-redux';
+import Footer from '@/Common/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   )
 }
