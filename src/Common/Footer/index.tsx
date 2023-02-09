e4b1021f512d5a2ba5b56553d1149e549dea624d
Prabhash Rai <raiprabh@groww.in>
2023-02-09 12:14:57 +0530

    created footer section

---
+++ src/Common/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from 'Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={style.footer67Section}>
+      
+    </div>
+  )
+}
+
+export default Footer;
