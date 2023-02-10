1f6de61d81b41d2d822de94429f37d8ce8ef37ff
Prabhash Rai <raiprabh@groww.in>
2023-02-10 11:35:35 +0530

    fixed list heading

--- src/UI/ProductListPage/ProductListHeading/index.tsx
+++ src/UI/ProductListPage/ProductListHeading/index.tsx
@@ -1,13 +1,17 @@
 import { QueryData } from '@/Common/Filters/Types';
+import { RootState } from '@/Store/store';
 import { extractData } from '@/utils/functions';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { getCategoriesString } from './getCategoriesString';
 import style from './ProductListHeading.module.css';
 
 const ProductListHeading = () => {
     const { pathname, query } = useRouter();
     const { value, sort, selectCategories, priceFilter } = query;
+
+    const showProducts = useSelector((state: RootState) => state.product.showList);
 
     const extractedValues: QueryData = {
         value: value,
@@ -17,7 +21,7 @@ const ProductListHeading = () => {
     }
 
     const { searchValue, sortBy, filterCategory, urlPrice } = extractData(extractedValues);
-    const sortString = sortBy !== "null" ? sortBy : ""; 
+    const sortString = sortBy !== "null" ? sortBy : "";
     const categoriesString = getCategoriesString(filterCategory);
 
     const heading = pathname === '/search' ? `Search Result` : `Showing All Products`;
@@ -29,34 +33,61 @@ const ProductListHeading = () => {
             {
                 pathname === '/search' &&
                 <>
-                    Applied Filters:-
                     <span className={style.product71Filters}>
                         {
                             <>
-                                <strong>Search Value : &nbsp; </strong> "{searchValue}" &emsp;
+                                {
+
+                                    searchValue &&
+                                    <span>
+                                        <strong>You searched for : &nbsp; </strong> "{searchValue}" &emsp;
+                                    </span>
+                                }
                             </>
                         }
                         {
                             <>
-                                <strong>Sort By : &nbsp; </strong> "{sortString}" &emsp;
+                                {
+                                    sortString &&
+                                    <span>
+                                        <strong>Sort By : &nbsp; </strong> "{sortString}" &emsp;
+                                    </span>
+                                }
                             </>
                         }
                         {
                             <>
-                                <strong>Categories : &nbsp; </strong>
-                                {categoriesString}
-                                &emsp;
+                                {
+                                    (categoriesString.length - 2) ?
+                                        <span>
+                                            <strong>Categories : &nbsp; </strong>
+                                            {categoriesString}
+                                            &emsp;
+                                        </span>
+                                        :
+                                        <></>
+                                }
                             </>
                         }
                         {
                             <>
-                                <strong>Price : &nbsp; </strong> "{urlPrice}"
+                                {
+                                    urlPrice &&
+                                    <span>
+                                        <strong>Price : &nbsp; </strong> "{urlPrice}"
+                                    </span>
+                                }
                             </>
                         }
+
                     </span>
+
                 </>
 
             }
+            <span className={style.list56TotalResult}>
+                Showing <strong> {showProducts?.length} </strong> results
+            </span>
         </div>
     )
 }
