61659e4b6afe313f89e9d6aac5effedb3ce34222
Prabhash Rai <raiprabh@groww.in>
2023-02-13 23:25:26 +0530

    fixed cart checkout

--- src/UI/CartPage/CartCheckout/index.tsx
+++ src/UI/CartPage/CartCheckout/index.tsx
@@ -11,17 +11,24 @@ const CartCheckout = () => {
     const cart = useSelector((state: RootState) => state.cart);
     const cartProducts = useSelector((state: RootState) => state.cart.cartProducts);
     const totalPrice = cart.totalCost.toFixed(2);
-    const cartSize = cartProducts.length;
 
     return (
         <div className={style.cart90Price}>
+            <h3 className={style.cart80Heading}>Cart Summary</h3>
             {
-                cartSize ?
-                    <h3>Items: &nbsp; {cartSize}</h3>
-                    :
-                    <></>
+                cartProducts.map((item) => {
+                    return (
+                        <div className={style.cart24Summary}>
+                            <div>
+                                <p>{item.product.title}</p>
+                                <p><b>Qnt: {item.quantity}</b></p>
+                            </div>
+                            <p><b>&#x20b9;{(item.quantity * item.product.price).toFixed(2)}</b></p>
+                        </div>
+                    )
+                })
             }
-            <h3>Total: &nbsp; {totalPrice}</h3>
+            <h4 className={style.cart89TotalPrice}>Total: &nbsp; &#x20b9; {totalPrice}</h4>
             <Button value={'Checkout'} className={style.cart23CheckoutButton} />
         </div>
     )
