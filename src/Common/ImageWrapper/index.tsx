a2e2956efc4c6be48cd70e634f09584c5f447673
Prabhash Rai <raiprabh@groww.in>
2023-02-13 15:57:20 +0530

    fixed image wrapper

--- src/Common/ImageWrapper/index.tsx
+++ src/Common/ImageWrapper/index.tsx
@@ -9,18 +9,26 @@ const ImageWrapper = ({ imageSrc, alt, blurHash }: ImageWrapperProps) => {
   const imageURL = `${imageSrc}`;
 
   const getBlurURL = () => {
-    const pixels = decode(blurHash, 32, 32)
-    const image = getImgFromArr(pixels, 32, 32)
-    const src = image.src;
-
-    return src;
+    if (typeof window !== "undefined") {
+      const pixels = decode(blurHash, 32, 32)
+      const image = getImgFromArr(pixels, 32, 32)
+      const src = image.src;
+      return src;
+    }
   }
 
   const blurImageSrc = useMemo(() => getBlurURL(), [imageSrc])
 
-
   return (
-    <Image src={imageURL} alt={alt} placeholder="blur" blurDataURL={blurImageSrc} unoptimized={true} width={100} height={100} />
+    <>
+      {
+        typeof window !== "undefined" ?
+          <Image src={imageURL} alt={alt} placeholder="blur" blurDataURL={blurImageSrc} unoptimized={true} width={100} height={100} />
+          :
+          <Image src={imageURL} alt={alt} unoptimized={true} width={100} height={100} />
+
+      }
+    </>
   )
 }
 
