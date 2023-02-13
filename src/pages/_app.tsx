6a6630942e3530bc3ff78325f219d396ea6322cb
Prabhash Rai <raiprabh@groww.in>
2023-02-13 15:27:35 +0530

    added progressbar for navigation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,15 @@ import { store } from '../Store/store';
 import { Provider } from 'react-redux';
 import Footer from '@/Common/Footer';
 
+//@ts-ignore
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { Router } from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
