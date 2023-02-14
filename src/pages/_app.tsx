bc86c027d4ff6ecf20675a4d78767dde4382d444
Prabhash Rai <raiprabh@groww.in>
2023-02-14 12:39:32 +0530

    fixed progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
-import Navbar from '@/Common/Navbar'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import { Router } from 'next/router';
+import type { AppProps } from 'next/app';
 
 import { store } from '../Store/store';
-import { Provider } from 'react-redux';
-import Footer from '@/Common/Footer';
 
 //@ts-ignore
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
-import { Router } from 'next/router';
 
-Router.events.on('routeChangeStart', () => NProgress.start()); 
-Router.events.on('routeChangeComplete', () => NProgress.done()); 
+import Footer from '@/Common/Footer';
+import Navbar from '@/Common/Navbar';
+
+import '@/styles/globals.css'
+
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 import { GetServerSideProps } from 'next/types';
 
 export default function Home() {
